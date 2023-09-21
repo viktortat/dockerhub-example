35ec07491af42a1b369ef0f59e2d6f803c7c1a7a
viktor <viktortat@gmail.com>
2023-09-22 00:44:39 +0300

    +

--- jenkins/login.sh
+++ jenkins/login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker login -u tavis1 dckr_pat_0vjOtCPrYMXsLwnRFOIXp29UxPM
 # echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-echo 'dckr_pat_0vjOtCPrYMXsLwnRFOIXp29UxPM' | docker login -u 'tavis1' --password-stdin
+echo 'dckr_pat_0vjOtCPrYMXsLwnRFOIXp29UxPM' | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
