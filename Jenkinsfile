95219c949af2f8484631fb8d808fc340425a7742
viktor <viktortat@gmail.com>
2023-09-22 00:33:34 +0300

    + up

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
     buildDiscarder(logRotator(numToKeepStr: '5'))
   }
   environment {
-    DOCKERHUB_CREDENTIALS_USR = 'tavis1'
+    DOCKERHUB_CREDENTIALS_USR = tavis1
     DOCKERHUB_CREDENTIALS_PSW = credentials('darinpope-dockerhub')
   }
   stages {
--- jenkins/login.sh
+++ jenkins/login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+docker login -u tavis1 dckr_pat_0vjOtCPrYMXsLwnRFOIXp29UxPM
 # echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
